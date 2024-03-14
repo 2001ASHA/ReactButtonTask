37f210293bc2fb568b4b5dad5295b69195095806
Asha R <asha_r@epam.com>
2024-03-14 15:10:57 +0530

    ButtonTask is implemented

---
+++ src/App.js
@@ -0,0 +1,13 @@
+import './App.css';
+import ButtonContainer from './component/buttonContainer';
+
+function App() {
+  return (
+    <div className="App">
+        <h1>Click the Button to see the text Box</h1>
+      <ButtonContainer/>
+    </div>
+  );
+}
+
+export default App;
